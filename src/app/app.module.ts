4c0f0da440e36bc6f31f331e68431774f126edc8
mailtodighade <mailtodighade@gmail.com>
2021-08-26 20:01:15 +0530

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,15 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { StudentController } from '../student/student.controller';
 import { TeacherController } from '../teacher/teacher.controller';
 import { TeacherStudents } from '../teacher/student.controller';
+import {UsersModule} from '../users/users.module'
+import { ContactsModule } from '../contacts/contacts.module';
+import {PhotoModule} from '../photo/photo.module'
 @Module({
-  imports: [],
+  imports: [ContactsModule],
   controllers: [
     AppController,
     StudentController,
---
+++ src/users/users.controller.ts
@@ -0,0 +1,13 @@
+import {  
+    Controller,
+    Get,
+    Post,
+    Patch,
+    Delete,
+    Body,
+    Param,
+    HttpStatus,
+  } from '@nestjs/common'; 
+
+@Controller('users')
+export class UsersController {}
